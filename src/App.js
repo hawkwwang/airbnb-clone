fee45bb62cfb3bd9adb013d8955c7946e1d07778
hawkwwang <hawkwwang@gmail.com>
2022-05-12 11:37:47 -0400

    v2.1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/airbnb-clone" element={<Home />}>            
+          <Route path="/" element={<Home />}>            
           </Route>
           <Route path="/search" element={<SearchPage />}>            
           </Route>
